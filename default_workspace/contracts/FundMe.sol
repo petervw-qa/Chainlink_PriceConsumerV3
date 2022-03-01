8430837e2cf5f9b8ef32a5621d5b90c793ef3564
Peter Vaughan-Williams <pvaughan-williams@qa.com>
2022-03-01 21:02:48 +0000

    uploading to remote repo

---
+++ default_workspace/contracts/FundMe.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+contract FundMe {
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+        // whats the ETH -> USD conversion rate
+    }
+}
